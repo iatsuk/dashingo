f1d1d64f601d940a22035f54665d9a9c3b2344f7
Andrey Yatsuk <yatsuk@megaputer.ru>
2017-12-26 14:52:53 +0300

    simplified script

--- js/board.js
+++ js/board.js
@@ -6,27 +6,30 @@ $(document).ready(function () {
     var grid = $('#grid');
     for (y = 1; y <= N; y++) {
         for (x = 1; x <= N; x++) {
+            var cell = $("<div style='grid-row: " + y + "; grid-column: + " + x + ";'></div>")
+                .addClass("unselectable grid-item");
             if (y === 1 && x === 1) {
-                grid.append("<div class='unselectable grid-item grid-top-left' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-top-left");
             } else if (y === 1 && x === N) {
-                grid.append("<div class='unselectable grid-item grid-top-right' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-top-right");
             } else if (y === N && x === 1) {
-                grid.append("<div class='unselectable grid-item grid-bottom-left' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-bottom-left");
             } else if (y === N && x === N) {
-                grid.append("<div class='unselectable grid-item grid-bottom-right' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-bottom-right");
             } else if (y === 1) {
-                grid.append("<div class='unselectable grid-item grid-top' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-top");
             } else if (y === N) {
-                grid.append("<div class='unselectable grid-item grid-bottom' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-bottom");
             } else if (x === 1) {
-                grid.append("<div class='unselectable grid-item grid-left' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-left");
             } else if (x === N) {
-                grid.append("<div class='unselectable grid-item grid-right' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-right");
             } else if (stars.includes(x) && stars.includes(y)) {
-                grid.append("<div class='unselectable grid-item grid-cross-dot' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-cross-dot");
             } else {
-                grid.append("<div class='unselectable grid-item grid-cross' style='grid-row: " + y + "; grid-column: + " + x + ";'></div>");
+                cell.addClass("grid-cross");
             }
+            grid.append(cell);
         }
     }
 });
