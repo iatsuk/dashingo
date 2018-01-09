e3ef7977ae4ca16ea91aae62bc9922a789c71566
Andrey Yatsuk <yatsuk@megaputer.ru>
2018-01-09 19:45:17 +0300

    get new stone from bowl

--- js/bowl.js
+++ js/bowl.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
-    var blackBowl = new Bowl('.bowl-black', '<div class="stone stone-black"></div>');
+    var blackBowl = new Bowl($('.bowl-black'), 'stone-black');
     blackBowl.draw();
 
-    var whiteBowl = new Bowl('.bowl-white', '<div class="stone stone-white"></div>');
+    var whiteBowl = new Bowl($('.bowl-white'), 'stone-white');
     whiteBowl.draw();
 });
 
-function Bowl(bowl_class, stone_html) {
-    var $bowl = $(bowl_class);
-    var stone = stone_html;
-
+function Bowl($bowl, stoneCls) {
     this.draw = function () {
         $bowl.droppable({
             accept: ".stone",
@@ -19,22 +16,24 @@ function Bowl(bowl_class, stone_html) {
             }
         });
 
-        $bowl.on('click', function () {
-            var $this = $(this);
-            var $stone = $(stone).css({
-                left: $this.offset().left + $this.width() / 2 + "px",
-                top: $this.offset().top + $this.height() / 2 + "px",
-                position: "absolute"
-            });
-            $stone.draggable({
-                zIndex: 100,
-                start: function (event, ui) {
-                    var $this = $(this);
-                    $this.css('position', 'absolute');
-                    $this.appendTo($('.container'));
-                }
-            });
-            $this.parent().parent().append($stone);
-        })
+        addFakeStone();
+    };
+
+    var addFakeStone = function () {
+        var $fakeStone = $('<div class="bowl-stone"></div>');
+        $fakeStone.draggable({
+            zIndex: 100,
+            start: function (event, ui) {
+                var $this = $(this);
+                $this.css('position', 'absolute')
+                    .appendTo($('.container'))
+                    .addClass("stone")
+                    .addClass(stoneCls)
+                    .removeClass("bowl-stone");
+                addFakeStone();
+            },
+            cursorAt: {left: $fakeStone.width() / 2, top: $fakeStone.height() / 2}
+        });
+        $bowl.append($fakeStone);
     }
 }
